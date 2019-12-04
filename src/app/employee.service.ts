e23241b0bb73cd9e48214c3cd18cb6b460e49884
ctolgaky <caglartolgaka@hotmail.com>
2019-12-04 22:40:58 +0300

    observables and error messages added

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,18 +1,25 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { IEmployee } from './employee';
-import { Observable } from 'rxjs';
+import {Observable, throwError } from 'rxjs';
+// import 'rxjs/add/operator/catch';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-private _url: string = '/assests/data/employees.json';
+private _url: string = '/assets/data/employees.json';
 
   constructor(private http: HttpClient) { }
 
   getEmployees(): Observable<IEmployee[]>{
-  return this.http.get<IEmployee[]>(this._url);
+  return this.http.get<IEmployee[]>(this._url)
+                  .pipe(catchError(this.errorHandler));
+  }
+
+  errorHandler(error: HttpErrorResponse){
+    return throwError(error.message || "Server Error");
   }
 }
